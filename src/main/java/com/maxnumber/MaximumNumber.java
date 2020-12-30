c991eca4b571ff83877de4207750b877d1df1782
sanket2498 <sanketdulange24@gmail.com>
2020-12-30 10:52:38 +0530

    Added : Maximum String value among three String values

--- src/main/java/com/maxnumber/MaximumNumber.java
+++ src/main/java/com/maxnumber/MaximumNumber.java
@@ -28,6 +28,17 @@ public class MaximumNumber <T>{
         printMax(x,y,z,max);
         return max;
     }
+    //UC3-Find Maximum String value among 3 string values
+    public static String testMaxString(String x, String y, String z)
+    {
+        String max=x;
+        if(y.compareTo(max)>0)
+            max=y;
+        if(z.compareTo(max)>0)
+            max=z;
+        printMax(x,y,z,max);
+        return max;
+    }
     public static void main(String args[])
     {
         System.out.println(testMaxInteger(90,40,50)); //Test Case-1.1
@@ -36,5 +47,9 @@ public class MaximumNumber <T>{
         System.out.println(testMaxFloat(90.56,40.45,50.22)); //Test Case-2.1
         System.out.println(testMaxFloat(10.55,90.6,40.22)); //Test Case-2.2
         System.out.println(testMaxFloat(20.44,50.22,90.67)); //Test Case-2.3
+        System.out.println(testMaxString("Peach","Apple","Banana")); //Test Case-3.1
+        System.out.println(testMaxString("Apple","Peach","Banana")); //Test Case-3.2
+        System.out.println(testMaxString("Banana","Apple","Peach")); //Test Case-3.3
+
     }
 }
