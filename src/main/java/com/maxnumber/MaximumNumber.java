d719c654bc1439ce9fd144e2ee4622f9aca3a4e9
sanket2498 <sanketdulange24@gmail.com>
2020-12-30 11:07:14 +0530

    Added : Multiple parameters to find the maximum value

--- src/main/java/com/maxnumber/MaximumNumber.java
+++ src/main/java/com/maxnumber/MaximumNumber.java
@@ -10,10 +10,38 @@ public class MaximumNumber <T>{
             max = z;
         return max;
     }
+    public static <E extends Comparable<E>> void MultipleParameters( E[] a)
+    {
+        E temp,x,y;
+        int n=a.length;
+        for(int i=0;i<n;i++)
+        {
+            for (int j=i+1;j<n;j++)
+            {
+                if(a[i].compareTo(a[j])>0)
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array is:");
+        for(int i=0;i< a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
     public static void main(String args[])
     {
         System.out.println("Maximum among three integers values:"+Maximum(20,30,40));
         System.out.println("Maximum among three string values:"+Maximum("ABC","SSD","WCD"));
         System.out.println("Maximum among three double values:"+Maximum(20.90,30.34,40.45));
+        Integer[] a={1,2,3,4,5,6};
+        String[] s={"san","ssd","abc","rty"};
+        Double[] d={11.09,20.59,12.45,23.67};
+        MultipleParameters(a);
+        MultipleParameters(s);
+        MultipleParameters(d);
     }
 }
